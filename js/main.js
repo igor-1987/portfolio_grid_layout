67497ec20c14b71f552cc4986ab1a22d7267d688
igor-1987 <igor.gayvoronsky@gmail.com>
2025-07-27 04:43:17 +0300

    finished layout

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
 const sidebar = document.querySelector("#sidebar");
 const navBtn = document.querySelector("#nav-btn");
 const closeBtn = document.querySelector("#close-btn");
+const date = document.querySelector("#date");
 
 // toggle navbar
 navBtn.addEventListener("click", () => {
   sidebar.classList.add("show-sidebar");
-})
+});
 
 closeBtn.addEventListener("click", () => {
-  sidebar.classList.remove("show-sidebar")
+  sidebar.classList.remove("show-sidebar");
+});
+
+// get current year
+date.innerHTML = new Date().getFullYear();
+
+// add fixed class to navbar
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 80) {
+    navbar.classList.add("navbar-fixed");
+  } else {
+    navbar.classList.remove("navbar-fixed");
+  }
 });
